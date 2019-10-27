01340b7e86493c801dac61145dc31133e02419b0
Carlos Arturo Vargas <carlosospino13@gmail.com>
2019-10-27 14:03:03 -0500

    Estilos CSS del menú

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="assets/css/style.css">
     <title>Proyecto 1 - PHP</title>
 </head>
 
@@ -19,13 +20,13 @@
         <!-- Menu -->
         <nav class="menu-nav">
             <ul>
-                <li><a href="index.php">Inicio</a></li>
-                <li><a href="index.php">Categoría 1</a></li>
-                <li><a href="index.php">Categoría 2</a></li>
-                <li><a href="index.php">Categoría 3</a></li>
-                <li><a href="index.php">Categoría 4</a></li>
-                <li><a href="index.php">Sobre mí</a></li>
-                <li><a href="index.php">Contacto</a></li>
+                <li class="menu-item"><a class="menu-link" href="index.php">Inicio</a></li>
+                <li class="menu-item"><a class="menu-link" href="index.php">Categoría 1</a></li>
+                <li class="menu-item"><a class="menu-link" href="index.php">Categoría 2</a></li>
+                <li class="menu-item"><a class="menu-link" href="index.php">Categoría 3</a></li>
+                <li class="menu-item"><a class="menu-link" href="index.php">Categoría 4</a></li>
+                <li class="menu-item"><a class="menu-link" href="index.php">Sobre mí</a></li>
+                <li class="menu-item"><a class="menu-link" href="index.php">Contacto</a></li>
             </ul>
         </nav>
 
