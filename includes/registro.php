9f07ef79c35a6e3bee20bb9e3a19de39adb47f2d
Carlos Arturo Vargas <carlosospino13@gmail.com>
2019-10-28 13:45:00 -0500

    Validación de campos para registrar usuarios

--- includes/registro.php
+++ includes/registro.php
@@ -1,16 +1,12 @@
 <?php
-if(isset($_POST['registrar'])) {
+if (isset($_POST['registrar'])) {
 
     // Recoger los valores del formulario de registro
-    $nombre    = isset($_POST['nombre']) ? $_POST['nombre'] : false; 
+    $nombre    = isset($_POST['nombre']) ? $_POST['nombre'] : false;
     $apellidos = isset($_POST['apellidos']) ? $_POST['apellidos'] : false;
-    $correo    = isset($_POST['emailRegister']) ? $_POST['emailRegister'] : false; 
-    $password  = isset($_POST['passwordRegister']) ? $_POST['passwordRegister'] : false; 
+    $email    = isset($_POST['emailRegister']) ? $_POST['emailRegister'] : false;
+    $password  = isset($_POST['passwordRegister']) ? $_POST['passwordRegister'] : false;
 }
-var_dump($nombre);
-var_dump($apellidos);
-var_dump($correo);
-var_dump($password);
 
 // Array de errores
 
@@ -18,12 +14,53 @@ $errores = [];
 
 // Validar los datos
 
-if(!empty($nombre) && !is_numeric($nombre) && !preg_match('/[0-9]/', $nombre)) {
+// Validar nombre
+
+if (!empty($nombre) && !is_numeric($nombre) && !preg_match('/[0-9]/', $nombre)) {
     $nombre_validate = true;
-    echo 'El nombre es válido';
+    echo '<h3>El nombre es válido</h3>';
 } else {
     $nombre_validate = false;
 
     $errores['nombre'] = 'Nombre inválido';
-
 }
+
+// Validar apellidos
+
+if (!empty($apellidos) && !is_numeric($apellidos) && !preg_match('/[0-9]/', $apellidos)) {
+    $apellidos_validate = true;
+    echo '<h3>El apellido es válido</h3>';
+} else {
+    $apellidos_validate = false;
+
+    $errores['apellidos'] = 'apellido inválido';
+}
+
+// Validar correo
+
+if (!empty($email) && filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    $email_validate = true;
+    echo '<h3>El email es válido</h3>';
+} else {
+    $email_validate = false;
+
+    $errores['email'] = 'El email no es válido';
+}
+
+if (!empty($password)) {
+    $password_validate = true;
+    echo '<h3>El password es válido</h3>';
+} else {
+    $password_validate = false;
+
+    $errores['password'] = 'password inválido';
+
+    echo '<h3>' . var_dump($errores) . '</h3>';
+}
+
+$guardar_usuario = false;
+
+if (count($errores) == 0) {
+    // Insertar en la BD
+    $guardar_usuario = true;
+} else { }
